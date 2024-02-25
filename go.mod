630316daae00146dbd4baff0a89b8ec67ac4c10e
paarthd00 <paarthcode@gmail.com>
2024-02-25 11:06:15 -0800

    add ai help

--- go.mod
+++ go.mod
@@ -15,6 +15,7 @@ require (
 	github.com/googleapis/enterprise-certificate-proxy v0.3.2 // indirect
 	github.com/googleapis/gax-go/v2 v2.12.1 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/sashabaranov/go-openai v1.20.0 // indirect
 	go.opencensus.io v0.24.0 // indirect
 	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.48.0 // indirect
 	go.opentelemetry.io/otel v1.23.0 // indirect
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"context"
 	"encoding/json"
 	"fmt"
 	"log"
@@ -10,6 +11,7 @@ import (
 	"strings"
 
 	"github.com/joho/godotenv"
+	"github.com/sashabaranov/go-openai"
 )
 
 type Item struct {
@@ -22,17 +24,85 @@ type GoogleResponse struct {
 	Items []Item `json:"items"`
 }
 
+// Replace with your actual key
+type ChatRequest struct {
+	Message string `json:"message"`
+}
+
 func main() {
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
+	fmt.Print("==============================================\n")
+	fmt.Print("Welcome to the Google Search Engine && AI Help\n")
+	fmt.Print("==============================================\n")
 
-	fmt.Println("Search Query::")
+	reader := bufio.NewReader(os.Stdin) // For efficient input reading
+
+	for {
+		fmt.Print("What type of help do you need? \n")
+		fmt.Print("1. Search for something\n")
+		fmt.Print("2. AI Help\n")
+		fmt.Print("3. Exit\n")
+
+		input, _ := reader.ReadString('\n') // Read until newline
+		input = strings.TrimSpace(input)    // Remove leading/trailing whitespace
+
+		switch input { // Use input directly
+		case "1":
+			search()
+		case "2":
+			aiHelp()
+		case "3":
+			fmt.Println("Exiting...")
+			return // Terminate the program
+		default:
+			fmt.Print("Invalid Option\n")
+		}
+	}
+}
+
+func aiHelp() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	fmt.Print("==============================================\n")
+	fmt.Print("Welcome to the AI Help\n")
+	fmt.Print("==============================================\n")
+	fmt.Print("Please enter your prompt\n")
+
+	reader := bufio.NewReader(os.Stdin)
+	aiPrompt, _ := reader.ReadString('\n')
+
+	client := openai.NewClient(os.Getenv("OPENAI_API_KEY"))
+
+	resp, err := client.CreateChatCompletion(
+		context.Background(),
+		openai.ChatCompletionRequest{
+			Model: openai.GPT3Dot5Turbo,
+			Messages: []openai.ChatCompletionMessage{
+				{
+					Role:    openai.ChatMessageRoleUser,
+					Content: aiPrompt,
+				},
+			},
+		},
+	)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("ChatCompletion response: %v\n", resp.Choices[0].Message.Content)
+}
+
+func search() {
+	fmt.Print("Search Query::")
 	// Read the search query from the user
 	reader := bufio.NewReader(os.Stdin)
 	searchQuery, _ := reader.ReadString('\n')
-	// format the search query
 	searchQuery = strings.Replace(searchQuery, " ", "+", -1)
 	searchQuery = strings.TrimSuffix(searchQuery, "\n")
 
