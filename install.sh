1780a4e25cac440c74ebfb4f1c2aadc520af65ed
paarthd00 <paarthcode@gmail.com>
2024-03-11 00:15:33 -0700

    redirect stderr to dev/null

--- install.sh
+++ install.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
+{
+  # Check for prerequisites (Go, etc.)
+  if ! command -v go &>/dev/null; then
+    echo "Go is not installed. Please install Go from https://go.dev/doc/install"
+    exit 1
+  fi
 
-# Check for prerequisites (Go, etc.)
-if ! command -v go &> /dev/null; then
-  echo "Go is not installed. Please install Go from https://go.dev/doc/install"
-  exit 1
-fi
+  INSTALL_DIR="${1:-/usr/local/bin}"
 
-INSTALL_DIR="${1:-/usr/local/bin}"
+  go build -o trms main.go
 
-go build -o trms main.go
+  sudo mv trms "$INSTALL_DIR"
 
-sudo mv trms "$INSTALL_DIR"
+  sudo chmod +x "$INSTALL_DIR/trms"
 
-sudo chmod +x "$INSTALL_DIR/trms"
+  PWD=$(pwd)
 
-PWD=$(pwd)
+  current_shell=$(echo $SHELL | awk -F '/' '{print $NF}')
 
-current_shell=$(echo $SHELL | awk -F '/' '{print $NF}')
+  echo "alias my-trm-search='my-trm-search -envfile $PWD/.env'" >>~/."$current_shell"rc
 
-echo "alias my-trm-search='my-trm-search -envfile $PWD/.env'" >> ~/."$current_shell"rc
+  echo "my-trm-search installed successfully in $INSTALL_DIR"
 
-echo "my-trm-search installed successfully in $INSTALL_DIR"
+} 2>/dev/null
